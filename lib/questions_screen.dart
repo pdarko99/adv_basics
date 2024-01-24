636763afbb36de05eec849a0eb429df7e5237803
pdarko99 <padarko99@gmail.com>
2024-01-24 09:02:50 +0000

    worked on displaying real qtsn on screen instead of placeholders

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,3 +1,5 @@
+import 'package:adv_basics/answer_button.dart';
+import 'package:adv_basics/data/questions.dart';
 import 'package:flutter/material.dart';
 
 class QuestionsScreen extends StatefulWidget {
@@ -11,19 +13,33 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsScreenState extends State<QuestionsScreen> {
   @override
   Widget build(BuildContext context) {
+    final currentQuestion = questions[0];
     return SizedBox(
       width: double.infinity,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          const Text('The question ....'),
-          const SizedBox(
-            height: 30,
-          ),
-          ElevatedButton(onPressed: () {}, child: const Text("Answer 1")),
-          ElevatedButton(onPressed: () {}, child: const Text("Answer 2")),
-          ElevatedButton(onPressed: () {}, child: const Text("Answer 3")),
-        ],
+      child: Container(
+        margin: const EdgeInsets.all(40),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              currentQuestion.text,
+              style: const TextStyle(color: Colors.white),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            ...currentQuestion.getShuffledAnswers().map(
+              (answer) {
+                return AnswerButton(
+                  answer,
+                  () {},
+                );
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
